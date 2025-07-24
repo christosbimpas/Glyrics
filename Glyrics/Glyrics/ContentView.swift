242cb3f57cf25ff88f2bd36ff33568d8f7365fa1
Christos Bimpas <christos.bimpas@icloud.com>
2025-07-25 00:23:16 +0300

    first commit

---
+++ Glyrics/Glyrics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glyrics
+//
+//  Created by Christos Bimpas on 25/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glyrics/Glyrics/GlyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlyricsApp.swift
+//  Glyrics
+//
+//  Created by Christos Bimpas on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct GlyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
